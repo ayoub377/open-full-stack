73d1370ab5bc1d5568a2d780c51f7fda65764944
ayutt <ayoubennaoui20@gmail.com>
2024-10-18 20:50:48 +0100

    ex 1.14 step 3

--- part1/anectodes/src/App.jsx
+++ part1/anectodes/src/App.jsx
@@ -13,17 +13,33 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [anecdote_votes, setanecdote_votes] = useState(Array(anecdotes.length).fill(0));
+  const [highest,sethighest] = useState(0)
 
   const generate_anecdote = ()=> {
    const generated = Math.floor(Math.random() * (anecdotes.length))
       setSelected(generated)
   }
 
-  return (
+    const handle_vote = () =>{
+        const anectodes_copy = [...anecdote_votes];
+        anectodes_copy[selected] += 1
+        setanecdote_votes(anectodes_copy)
+        const highest_vote = Math.max(...anecdote_votes)
+        const index = anecdote_votes.indexOf(highest_vote)
+        console.log(index)
+        sethighest(index)
+    }
 
+  return (
       <div>
-        {anecdotes[selected]}
-        <button onClick={generate_anecdote}>next anecdote</button>
+        {anecdotes[selected]} has {anecdote_votes[selected]}
+          <br/>
+        <button onClick={handle_vote} style={{backgroundColor:"green"}}>vote</button>
+        <button onClick={generate_anecdote} style={{backgroundColor:"red"}}>next anecdote</button>
+          <br/>
+          <h2>Anecdote With most votes</h2>
+          {anecdotes[highest]} has {anecdote_votes[highest]}
       </div>
   )
 }
