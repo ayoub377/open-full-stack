1d9dd8428688420631344d6cfab0a5b814f25a4c
ayutt <ayoubennaoui20@gmail.com>
2024-10-18 13:02:38 +0100

    refactored

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 
-const Statistics = ({good,neutral,bad,all,average,positive}) => {
+const Statistics = ({good,neutral,bad}) => {
+    const total = good+bad+neutral
+    const getAverage = (good - bad) / total
+    const positive = (good / total) * 100 || 0;
+
     return (
         <div>
             <p>Statistics</p>
             <div>
-                {all > 0 ? (
+                {total > 0 ? (
                     <div>
                         <p>good {good}</p>
                         <p>neutral {neutral}</p>
                         <p>bad {bad}</p>
-                        <p>All {all}</p>
-                        <p>Average {average}</p>
+                        <p>All {total}</p>
+                        <p>Average {getAverage}</p>
                         <p>positive {positive} %</p>
                     </div>
 
@@ -29,37 +33,23 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const [all, setAll] = useState(0)
-      const [average, setAverage] = useState(0)
 
     const handleGood = () => {
           const good_score = good + 1
-          const newAll = all +1
           setGood(good_score)
-          setAll(newAll)
-          const average_score = (good_score - bad) / newAll
-          setAverage(average_score)
     }
 
     const handleNeutral = () => {
         const neutral_score = neutral + 1
-        const newAll = all + 1
         setNeutral(neutral_score)
-        setAll(newAll)
-        const average_score = (good - bad) / newAll
-            setAverage(average_score)
     }
 
     const handleBad = () => {
         const  bad_score = bad + 1
-        const newAll = all + 1
         setBad(bad_score)
-        setAll(newAll)
-        const average_score = (good-bad_score) / newAll
-        setAverage(average_score)
     }
 
-    const positive = (good / all) * 100 || 0;
+
 
     return (
     <div>
@@ -69,7 +59,7 @@ const App = () => {
             <button onClick={handleNeutral}>neutral</button>
             <button onClick={handleBad}>bad</button>
         </div>
-        <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
